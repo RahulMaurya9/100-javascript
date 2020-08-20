6dccea308c3c88b3f7818288f6cff3d9e519a6c6
unknown <rahulmaurya032@gmail.com>
2020-08-20 15:34:41 +0530

    Challenge 1

---
+++ Challenge1/index.js
@@ -0,0 +1,7 @@
+var data = document.querySelector('#butt')
+var back = document.querySelector('#background')
+
+data.addEventListener('click', function(){
+    back.style.backgroundColor = "green";
+
+})
